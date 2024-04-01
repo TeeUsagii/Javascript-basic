d9691cf3bd194fce8ec806e30e1e2714e60128ef
TeeUsagii <tiennguyen2003.b@gmail.com>
2024-04-01 22:14:32 +0700

    exam comment  in js

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -1,7 +1,24 @@
 // alert('Hi Javascript basic')
+
+// Khai bao ten, tuoi
 var fullName = 'Kaneki Ken';
 var age = 26;
 
-
+// Goi ham thong bao
 alert(fullName);
 alert(age);
+
+//Comment dung de ghi chu
+//Comment dung de vo hieu hoa ma
+
+//Co hai loai comment
+/*
+
+- Comment 1 dong 
+- Comment nhieu dong
+
+*/
+
+/** 
+ * Cai nay dung /** la se tu dong goi y
+*/
