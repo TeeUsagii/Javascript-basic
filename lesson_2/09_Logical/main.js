9ef5d8bc18fbc19e8f9ce5ce7070bc7aa95cd133
TeeUsagii <tiennguyen2003.b@gmail.com>
2024-04-02 17:23:23 +0700

    Logical

---
+++ lesson_2/09_Logical/main.js
@@ -0,0 +1,21 @@
+/**
+ * Logic operators
+ * && - and
+ * || - or
+ * ! - Not
+ */
+
+var a = 1;
+var b = 2;
+var c = 3;
+
+// if a > 0 and b > 0 or c > 0 
+//print 'Dieu kien dung'
+if (a > 0 && b > 0 || c > 0) {
+    console.log('Dieu kien dung')
+}
+
+// not print
+if (!(a > 0)) {
+    console.log('Dieu kien dung');
+}
