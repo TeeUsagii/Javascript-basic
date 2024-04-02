f23fd186dfd1f486d8e01eb37365781e5c57d699
TeeUsagii <tiennguyen2003.b@gmail.com>
2024-04-02 16:13:30 +0700

    Arithmetic and Assignment

---
+++ lesson_2/02_Arithmetic_operators/main.js
@@ -0,0 +1,15 @@
+/**
+ + Cộng
+ - Trừ
+ * Nhân
+ ** Lũy thừa
+  / Chia
+  % Chia lấy số dư
+  ++ Tăng một giá trị số
+  -- Giảm một giá trị số
+ */
+
+var a = 5;
+var b = 2;
+var c = a + b;
+console.log(c);
