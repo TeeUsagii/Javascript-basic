a578180a708c6b860b570a5f976dc4e46b6510e1
TeeUsagii <tiennguyen2003.b@gmail.com>
2024-04-02 15:47:00 +0700

    Lesson 2 operator

---
+++ lesson_2/01_Operator/main.js
@@ -0,0 +1,29 @@
+/**
+ * Toán tử số học - Arithmetic
+ * Toán tử gán - Assignment
+ * Toán tử so sánh - Comparison
+ * Toán tử logic - Logical
+ */
+
+
+// Toán tử số học - Arithmetic
+var a = 1 + 2;
+console.log(a);
+
+// Toán tử gán
+var fullName = 'Kaneki';
+console.log(fullName);
+
+
+// Toán tử so sánh
+var a = 1;
+var b = 2;
+
+if(a > b) {
+    alert('Yes')
+}
+
+// Toán tử logic
+if(a > 0 && b > 0) {
+    alert('a & b lớn hơn 0')
+}
