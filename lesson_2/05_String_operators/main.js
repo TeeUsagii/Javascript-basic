133cd5fdf0e23ab369cac4972ea5ee7a8ebe6d07
TeeUsagii <tiennguyen2003.b@gmail.com>
2024-04-02 16:39:31 +0700

    String operator

---
+++ lesson_2/05_String_operators/main.js
@@ -0,0 +1,14 @@
+/**
+ * Toán tử chuỗi - String operator
+ */
+
+// var fistName = 'Son';
+// var lastName = 'Tung';
+// console.log(fistName + ' ' + lastName);
+
+var fistName = 'Son';
+// fistName = fistName + 'Tung' 
+fistName += ' Tung' 
+
+console.log(fistName);
+
