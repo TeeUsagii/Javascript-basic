3bad8bc39677f3a9f0e51e9079c8fc2c730b9a61
TeeUsagii <tiennguyen2003.b@gmail.com>
2024-04-02 17:31:56 +0700

    more exam with logical

--- lesson_2/09_Logical/main.js
+++ lesson_2/09_Logical/main.js
@@ -9,13 +9,31 @@ var a = 1;
 var b = 2;
 var c = 3;
 
+var d = true || false
+var e = false || true
+var f = true && false
+
+console.log(d)
+console.log(e)
+console.log(f)
+
 // if a > 0 and b > 0 or c > 0 
 //print 'Dieu kien dung'
-if (a > 0 && b > 0 || c > 0) {
-    console.log('Dieu kien dung')
-}
+// if (a > 0 && b > 0 || c > 0) {
+//     console.log('Dieu kien dung')
+// }
 
 // not print
 if (!(a > 0)) {
+    console.log('Dieu kien dung');
+}
+
+//not print
+if (d > a) {
+    console.log('Dieu kien dung');
+}
+
+//print
+if (d == true) {
     console.log('Dieu kien dung');
 }
