184f55d0db8ebd7599a90297534b632a178e80ba
TeeUsagii <tiennguyen2003.b@gmail.com>
2024-04-02 16:51:22 +0700

    Comparison operators

---
+++ lesson_2/06_Comparison_operators/main.js
@@ -0,0 +1,28 @@
+/**
+ * Toán tử so sánh - Comparison operators
+ * == --> bằng
+ * != --> khác
+ * > --> lớn hơn
+ * < --> nhỏ hơn
+ * >= --> lớn hơn hoặc bằng
+ * <= --> nhỏ hơn hoặc bằng
+ */
+
+
+// var a = 1;
+// var b = 2;
+
+// if(a != b) {
+//     console.log('Dieu kien dung');
+// } else {
+//     console.log('Dieu kien sai');
+// }
+
+var a = 'Hi';
+var b = 'Hi';
+
+if(a == b) {
+    console.log('Dieu kien dung');
+} else {
+    console.log('Dieu kien sai');
+}
