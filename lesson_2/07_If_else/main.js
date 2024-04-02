24af846aa8bbbcb96d9d884af90241c877176a7a
TeeUsagii <tiennguyen2003.b@gmail.com>
2024-04-02 17:11:59 +0700

    If_else

---
+++ lesson_2/07_If_else/main.js
@@ -0,0 +1,56 @@
+/**
+ * If - else
+ */
+
+/**
+ * Convert value to false
+ * 0
+ * false
+ * '' or ""
+ * undefined
+ * NaN
+ * null
+ */
+
+// True
+if (true) {
+    console.log('Dieu kien dung')
+} else {
+    console.log('Dieu kien sai')
+}
+
+// False
+if (false) {
+    console.log('Dieu kien dung')
+} else {
+    console.log('Dieu kien sai')
+}
+
+// True
+var isSuccess = 1 < 2;
+
+if (isSuccess) {
+    console.log('Dieu kien dung')
+} else {
+    console.log('Dieu kien sai')
+}
+
+// True
+var fullName = 'Son Tung';
+
+// js convert fullName to boolean
+if (fullName) {
+    console.log('Dieu kien dung')
+} else {
+    console.log('Dieu kien sai')
+}
+
+//False
+var number = 0;
+
+// js convert fullName to boolean
+if (number) {
+    console.log('Dieu kien dung')
+} else {
+    console.log('Dieu kien sai')
+}
