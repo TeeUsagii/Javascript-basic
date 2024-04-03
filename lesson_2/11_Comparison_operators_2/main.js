8a7a798e0cab9cccd1f1f552811e5bfe1941c640
TeeUsagii <tiennguyen2003.b@gmail.com>
2024-04-03 14:55:03 +0700

    11_Comparison_operators_2

---
+++ lesson_2/11_Comparison_operators_2/main.js
@@ -0,0 +1,15 @@
+/**
+ * Toán tử so sánh 2 - Comparison operators 2
+ * ===
+ * !==
+ */
+
+var a = 1;
+var b = '1';
+
+console.log(a == b);// true, so sánh value
+console.log(a === b);// false, so sánh value, datatype
+
+console.log(a != b);// false, so sánh value
+console.log(a !== b);// false, so sánh value, datatype
+
