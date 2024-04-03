ee2315bf85f3d7099393c925a813820f87d2e12b
TeeUsagii <tiennguyen2003.b@gmail.com>
2024-04-03 15:29:35 +0700

    Function define

---
+++ lesson_3/01_Function/main.js
@@ -0,0 +1,19 @@
+/**
+ * 1.Hàm - Function
+ * - Một khối mã
+ * - Làm một việc cụ thể
+ * 2. Loại hàm
+ * - Built-in
+ * - Tự định nghĩa
+ * 3. Tính chất
+ * - Không thực thi khi định nghĩa
+ * - Sẽ thực thi khi được gọi
+ * - Có thể nhận tham số
+ * - Có thể trả về một giá trị
+ */
+
+function showDiaglog() {
+    alert('Show diaglog');
+}
+
+showDiaglog();
