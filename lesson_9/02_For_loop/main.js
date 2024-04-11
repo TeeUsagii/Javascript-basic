460415d6c0393dd41ba690e9dfc9e1fce98bec56
TeeUsagii <tiennguyen2003.b@gmail.com>
2024-04-11 16:20:03 +0700

    For 3

--- lesson_9/02_For_loop/main.js
+++ lesson_9/02_For_loop/main.js
@@ -2,6 +2,20 @@
  * For loop
  */
 
-for (var i = 1; i <= 1000; i++) {
-    console.log(i);
+var myArray = [
+    'Javascript',
+    'PHP',
+    'Java',
+    'Dart',
+    'Python'
+];
+
+// for (var i = 1; i <= 1000; i++) {
+//     console.log(i);
+// }
+
+var arrayLength = myArray.length;
+
+for (var i = 0; i < arrayLength; i++) {
+    console.log(myArray[i]);
 }
