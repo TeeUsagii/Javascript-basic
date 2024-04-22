bf24559eb66c79b37e7924c581ce37b845a51cdd
TeeUsagii <tiennguyen2003.b@gmail.com>
2024-04-22 17:35:02 +0700

    reduce logic

--- lesson_10/03_Reduce_logic/main.js
+++ lesson_10/03_Reduce_logic/main.js
@@ -1,15 +1,23 @@
 Array.prototype.reduce2 = function (callback, result) {
-        for(let i = 0; i < this.length; i++) {
-          result = callback(result, this[i], i, this);
-        }
-        return result;
+  let i = 0
+  
+  if (arguments.length < 2) {
+    i = 1
+    result = this[0]
+  }
+
+  for (; i < this.length; i++) {
+    result = callback(result, this[i], i, this);
+  }
+  return result;
 }
 
 const numbers = [1, 2, 3, 4, 5]
 
-const result = numbers.reduce2((total, number) => {
-    return total + number
-},);
+const result = numbers.reduce2((total, number, index, arrray) => {
+  console.log(total, number, index, arrray)
+  return total + number
+})
 
 console.log(result)
 
