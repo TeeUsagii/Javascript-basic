aea8b7296845fd5f1dee5e0e45ab61c5183a32e5
TeeUsagii <tiennguyen2003.b@gmail.com>
2024-05-07 19:01:16 +0700

    exam

--- lesson_12/09_InnerHTML_OuterHTML/main.js
+++ lesson_12/09_InnerHTML_OuterHTML/main.js
@@ -2,18 +2,39 @@
  * InnerHTML vs OuterHTML Property
  */
 
-var boxElement = document.querySelector('.box');
+// var boxElement = document.querySelector('.box');
 
-boxElement.innerHTML = '<h1 class="heading">Heading</h1>';
+// boxElement.innerHTML = '<h1 class="heading">Heading</h1>';
 
-console.log(document.querySelector('.heading').innerText);
+// console.log(document.querySelector('.heading').innerText);
 
-console.log(boxElement.innerHTML);
-
-console.log(boxElement.outerHTML);
-
-boxElement.outerHTML = '<span>Day la span</span>'
+// console.log(boxElement.innerHTML);
 
 // console.log(boxElement.outerHTML);
 
+// boxElement.outerHTML = '<span>Day la span</span>'
 
+// // console.log(boxElement.outerHTML);
+
+
+var courses = ['HTML & CSS', 'Javascript', 'PHP', 'Java']
+
+function render(courses) {
+    var chatLu = document.querySelector('ul');
+    var leng = courses.length;
+    for (var i = 0; i < leng; i++) {
+        chatLu.innerHTML += `<li>${courses[i]}</li>`;
+    }
+}
+
+render(courses)
+
+function render2(courses) {
+    var chatLu = document.querySelector('ul');
+    var leng = courses.length;
+    var chatVai = courses.map(function(course) {
+        return "<li>courses</li>"
+    });
+}
+
+render(courses)
